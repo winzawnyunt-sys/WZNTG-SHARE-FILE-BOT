9827df73d296f890e93deeff4c0b186a6e366f9c
winzawnyunt <winzawnyunt@gmail.com>
2026-06-02 18:14:05 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import asyncio
+from bot import Bot
+from pyrogram import idle
 
+async def main():
+    app = Bot()
+    await app.start()
+    await idle()
+
+if __name__ == "__main__":
+    asyncio.run(main())
