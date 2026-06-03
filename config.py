c6714c04380672820dffa4ad44b3cdfa69279786
winzawnyunt <winzawnyunt@gmail.com>
2026-06-03 09:32:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@
 API_ID = 31778137 
 API_HASH = "357247ed45b28459c6600e31cce5b773"
 TOKEN = "7049303682:AAHD3DCUYjhAQCHGYH5FhzMySEW-c-d8SlU"
+MONGO_DB_URL = "mongodb+srv://kosanlinyarhmon_db_user:bg7dDn6e5enfhP1z@cluster0.fnbnxry.mongodb.net/?appName=Cluster0"
